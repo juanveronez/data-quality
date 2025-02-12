3e5793d073c834843dd5f2030f1c9a12fdd80d28
Juan Veronez <juan.monteirov@gmail.com>
2025-02-12 12:28:35 -0300

    Add pydantic and pandera to data validation and create some exemples of use

---
+++ exemples/exemple01.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from pydantic import BaseModel, ValidationError
+
+class Delivery(BaseModel):
+    timestamp: datetime
+    dimensions: tuple[int, int]
+
+d = Delivery(timestamp='2025-02-12T11:45:34Z', dimensions=['10', '20'])
+print(d.model_dump())
+
+try:
+    o = {"timestamp": "test", "dimensions": [10, 20]}
+    Delivery(**o)
+except ValidationError as e:
+    print(e)
+    
---
+++ exemples/exemple03_ref_exemple00.py
@@ -0,0 +1,18 @@
+from pydantic import validate_call, PositiveInt, ValidationError
+
+@validate_call
+def soma(x: PositiveInt, y: PositiveInt) -> PositiveInt:
+    return x + y
+
+print(soma(3,2))
+print(soma(3,5))
+
+try:
+    soma(3,-2)
+except ValidationError as e:
+    print(e)
+
+try:
+    soma(3,'a')
+except ValidationError as e:
+    print(e)
